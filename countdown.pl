21ee6d4ea59ef9cebe448bb48b3c9e467682f4a6
seth <email_git_7624@wg-karlsruhe.de>
2017-02-26 14:08:26 +0100

    fix link

--- countdown.pl
+++ countdown.pl
@@ -394,7 +394,7 @@ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-originally written by seth (see https://github.com/wp-seth/camelbot)
+originally written by seth (see https://github.com/wp-seth)
 
 =cut
 
